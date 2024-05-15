adfdd47dfb9012973c78bfe80d9a0408290484e1
stephenpozo <stephen.pozo@pluto.tv>
2024-05-15 10:18:47 -0500

    update buildspec file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('testing second scenario',()=>{
+    cy.visit('google.com')
+  })
 })
